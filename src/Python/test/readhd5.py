776070e22bf95491275a023f3a5ac00cea356714
Edoardo Pasca <edo.paskino@gmail.com>
2017-10-11 15:12:42 +0100

    read and plot the hdf5

--- src/Python/test/readhd5.py
+++ src/Python/test/readhd5.py
@@ -26,3 +26,16 @@ angles_rad = numpy.asarray(nx.get('/angles_rad'))
 recon_size = numpy.asarray(nx.get('/recon_size'), dtype=int)[0]
 size_det = numpy.asarray(nx.get('/size_det'), dtype=int)[0]
 slices_tot = numpy.asarray(nx.get('/slices_tot'), dtype=int)[0]
+
+#from ccpi.viewer.CILViewer2D import CILViewer2D
+#v = CILViewer2D()
+#v.setInputAsNumpy(Weights3D)
+#v.startRenderLoop()
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
+a=fig.add_subplot(1,1,1)
+a.set_title('noise')
+imgplot = plt.imshow(Weights3D[0].T)
+plt.show()
