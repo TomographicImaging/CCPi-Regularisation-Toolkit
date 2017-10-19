dd6e415991f312bf54cdd69d1dd09fb8bcdebd2a
Edoardo Pasca <edo.paskino@gmail.com>
2017-10-19 12:38:58 +0100

    moved code out of if-the-else
    closes issue #5

--- main_func/FISTA_REC.m
+++ main_func/FISTA_REC.m
@@ -517,6 +517,18 @@ else
         
         % subsets loop
         counterInd = 1;
+		if (strcmp(proj_geom.type,'parallel') || strcmp(proj_geom.type,'fanflat') || strcmp(proj_geom.type,'fanflat_vec'))
+			% if geometry is 2D use slice-by-slice projection-backprojection routine                    
+			for kkk = 1:SlicesZ
+				[sino_id, sinoT] = astra_create_sino_cuda(X_t(:,:,kkk), proj_geomSUB, vol_geom);
+				sino_updt_Sub(:,:,kkk) = sinoT';
+				astra_mex_data2d('delete', sino_id);
+			end
+		else
+			% for 3D geometry (watch the GPU memory overflow in earlier ASTRA versions < 1.8)
+			[sino_id, sino_updt_Sub] = astra_create_sino3d_cuda(X_t, proj_geomSUB, vol_geom);
+			astra_mex_data3d('delete', sino_id);
+		end
         for ss = 1:subsets
             X_old = X;
             t_old = t;         
@@ -528,18 +540,7 @@ else
             
             if (lambdaR_L1 > 0)
                 % Group-Huber fidelity (ring removal)
-                if (strcmp(proj_geom.type,'parallel') || strcmp(proj_geom.type,'fanflat') || strcmp(proj_geom.type,'fanflat_vec'))
-                    % if geometry is 2D use slice-by-slice projection-backprojection routine                    
-                    for kkk = 1:SlicesZ
-                        [sino_id, sinoT] = astra_create_sino_cuda(X_t(:,:,kkk), proj_geomSUB, vol_geom);
-                        sino_updt_Sub(:,:,kkk) = sinoT';
-                        astra_mex_data2d('delete', sino_id);
-                    end
-                else
-                    % for 3D geometry (watch the GPU memory overflow in earlier ASTRA versions < 1.8)
-                    [sino_id, sino_updt_Sub] = astra_create_sino3d_cuda(X_t, proj_geomSUB, vol_geom);
-                    astra_mex_data3d('delete', sino_id);
-                end
+                
                 
                 residualSub = zeros(Detectors, numProjSub, SlicesZ,'single'); % residual for a chosen subset
                 for kkk = 1:numProjSub
@@ -550,18 +551,7 @@ else
                 
             elseif (studentt > 0)
                 % student t data fidelity
-                if (strcmp(proj_geom.type,'parallel') || strcmp(proj_geom.type,'fanflat') || strcmp(proj_geom.type,'fanflat_vec'))
-                    % if geometry is 2D use slice-by-slice projection-backprojection routine
-                    for kkk = 1:SlicesZ
-                        [sino_id, sinoT] = astra_create_sino_cuda(X_t(:,:,kkk), proj_geomSUB, vol_geom);
-                        sino_updt_Sub(:,:,kkk) = sinoT';
-                        astra_mex_data2d('delete', sino_id);
-                    end
-                else
-                    % for 3D geometry (watch the GPU memory overflow in earlier ASTRA versions < 1.8)
-                    [sino_id, sino_updt_Sub] = astra_create_sino3d_cuda(X_t, proj_geomSUB, vol_geom);
-                    astra_mex_data3d('delete', sino_id);
-                end
+                
                 
                 % artifacts removal with Students t penalty
                 residualSub = squeeze(weights(:,CurrSubIndeces,:)).*(sino_updt_Sub - squeeze(sino(:,CurrSubIndeces,:)));
@@ -576,21 +566,7 @@ else
                 objective(i) = ff; % for the objective function output
             else
                 % PWLS model
-                if (strcmp(proj_geom.type,'parallel') || strcmp(proj_geom.type,'fanflat') || strcmp(proj_geom.type,'fanflat_vec'))
-                    % if geometry is 2D use slice-by-slice projection-backprojection routine
-                    for kkk = 1:SlicesZ
-                        [sino_id, sinoT] = astra_create_sino_cuda(X_t(:,:,kkk), proj_geomSUB, vol_geom);
-                        sino_updt_Sub(:,:,kkk) = sinoT';
-                        astra_mex_data2d('delete', sino_id);
-                    end
-                else
-                    % for 3D geometry (watch the GPU memory overflow in earlier ASTRA versions < 1.8)
-                    [sino_id, sino_updt_Sub] = astra_create_sino3d_cuda(X_t, proj_geomSUB, vol_geom);
-                    astra_mex_data3d('delete', sino_id);
-                end
-                residualSub = squeeze(weights(:,CurrSubIndeces,:)).*(sino_updt_Sub - squeeze(sino(:,CurrSubIndeces,:)));
-                objective(i) = 0.5*norm(residualSub(:)); % for the objective function output
-            end
+                
             
             if (strcmp(proj_geom.type,'parallel') || strcmp(proj_geom.type,'fanflat') || strcmp(proj_geom.type,'fanflat_vec'))
                 % if geometry is 2D use slice-by-slice projection-backprojection routine
