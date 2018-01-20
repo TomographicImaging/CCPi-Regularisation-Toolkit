ba6c5455f4095dcd8a3a2262aa253fac9bb9ba09
Edoardo Pasca <edo.paskino@gmail.com>
2018-01-20 22:41:24 +0000

    work for conda recipes

--- Wrappers/Python/conda-recipe/bld.bat
+++ Wrappers/Python/conda-recipe/bld.bat
@@ -4,8 +4,8 @@ exit 1
 )
 
 mkdir "%SRC_DIR%\ccpi"
-xcopy /e "%RECIPE_DIR%\..\.." "%SRC_DIR%\ccpi"
-
+ROBOCOPY /E "%RECIPE_DIR%\..\.." "%SRC_DIR%\ccpi"
+ROBOCOPY /E "%RECIPE_DIR%\..\..\..\Core" "%SRC_DIR%\Core"
 cd %SRC_DIR%\ccpi\Python
 
 %PYTHON% setup.py build_ext
