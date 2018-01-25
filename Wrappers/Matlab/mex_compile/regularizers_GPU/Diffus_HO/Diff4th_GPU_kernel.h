ef41d45779400ae029a14822df79d5d8910ef03d
algol <dkazanc@hotmail.com>
2018-01-25 17:21:37 +0000

    #15 solves dependencies on core C/CUDA functions for Matlab, all duplicates deleted

--- Wrappers/Matlab/mex_compile/regularizers_GPU/Diffus_HO/Diff4th_GPU_kernel.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DIFF_HO_H_
-#define __DIFF_HO_H_
-
-extern "C" void Diff4th_GPU_kernel(float* A, float* B, int N, int M, int Z, float sigma, int iter, float tau, float lambda);
-
-#endif 
--- Wrappers/Matlab/mex_compile/regularizers_GPU/NL_Regul/NLM_GPU_kernel.h
+++
@@ -1,6 +0,0 @@
-#ifndef __NLMREG_KERNELS_H_
-#define __NLMREG_KERNELS_H_
-
-extern "C" void NLM_GPU_kernel(float *A, float* B, float *Eucl_Vec, int N, int M, int Z, int dimension, int SearchW, int SimilW, int SearchW_real, float denh2, float lambda);
-
-#endif 
