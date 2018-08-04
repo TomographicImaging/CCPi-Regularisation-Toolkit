bd874abcebae787ea1f1fddc138fb67a8abf9c9b
Edoardo Pasca <edo.paskino@gmail.com>
2018-08-04 23:06:25 +0100

    working CMake project for Win and conda/no-conda

--- Wrappers/Python/conda-recipe/bld.bat
+++ Wrappers/Python/conda-recipe/bld.bat
@@ -6,12 +6,15 @@ exit 1
 mkdir "%SRC_DIR%\ccpi"
 ROBOCOPY /E "%RECIPE_DIR%\..\.." "%SRC_DIR%\ccpi"
 ROBOCOPY /E "%RECIPE_DIR%\..\..\..\Core" "%SRC_DIR%\Core"
-cd %SRC_DIR%\ccpi\Python
+::cd %SRC_DIR%\ccpi\Python
+cd %SRC_DIR%
 
 :: issue cmake to create setup.py
-cmake . 
+cmake -G "NMake Makefiles" %RECIPE_DIR%\..\..\..\ -DBUILD_WRAPPERS=ON -DCONDA_BUILD=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB="%CONDA_PREFIX%\lib" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library"
 
-%PYTHON% setup-regularisers.py build_ext
-if errorlevel 1 exit 1
-%PYTHON% setup-regularisers.py install
+::%PYTHON% setup-regularisers.py build_ext
+::if errorlevel 1 exit 1
+::%PYTHON% setup-regularisers.py install
+::if errorlevel 1 exit 1
+nmake install
 if errorlevel 1 exit 1
