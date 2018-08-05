2a2eb0e083991753a8d9247b63fea8f37559a6d7
Edoardo Pasca <edo.paskino@gmail.com>
2018-08-05 15:25:41 +0100

    updated conda recipe build.sh

--- Wrappers/Python/conda-recipe/build.sh
+++ Wrappers/Python/conda-recipe/build.sh
@@ -7,13 +7,14 @@ mkdir "$SRC_DIR/ccpi"
 cp -rv "$RECIPE_DIR/../.." "$SRC_DIR/ccpi"
 cp -rv "$RECIPE_DIR/../../../Core" "$SRC_DIR/Core"
 
-cd $SRC_DIR/ccpi/Python
+cd $SRC_DIR
 
-echo "$SRC_DIR/ccpi/Python"
+cmake -G "NMake Makefiles" $RECIPE_DIR/../../../ -DBUILD_WRAPPERS=ON -DCONDA_BUILD=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=$CONDA_PREFIX/lib -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX/Library"
 
-cmake . 
 
-$PYTHON setup-regularisers.py build_ext
-$PYTHON setup-regularisers.py install
+make install
+
+#$PYTHON setup-regularisers.py build_ext
+#$PYTHON setup-regularisers.py install
 
 
