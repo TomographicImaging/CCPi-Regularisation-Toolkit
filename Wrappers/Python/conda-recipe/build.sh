20f82364390ed7a9cdd544ac2e9ad42779abbe70
Edoardo Pasca <edo.paskino@gmail.com>
2018-08-15 08:50:50 +0100

    removed CIL_VERSION env variable from conda build

--- Wrappers/Python/conda-recipe/build.sh
+++ Wrappers/Python/conda-recipe/build.sh
@@ -1,8 +1,4 @@
 
-if [ -z "$CIL_VERSION" ]; then
-    echo "Need to set CIL_VERSION"
-    exit 1
-fi  
 mkdir "$SRC_DIR/ccpi"
 cp -rv "$RECIPE_DIR/../.." "$SRC_DIR/ccpi"
 cp -rv "$RECIPE_DIR/../../../Core" "$SRC_DIR/Core"
