ed7061b4c0ee0f225850ae95cb149773928f757e
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2018-12-07 00:52:58 +0000

    return insteadd of exit

--- Core/regularisers_GPU/LLT_ROF_GPU_core.cu
+++ Core/regularisers_GPU/LLT_ROF_GPU_core.cu
@@ -44,7 +44,7 @@ limitations under the License.
 {                                                                              \
     const cudaError_t error = call;                                            \
     if (error != cudaSuccess)                                                  \
-    {                                 /                                         \
+    {                                                                          \
         fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);                 \
         fprintf(stderr, "code: %d, reason: %s\n", error,                       \
                 cudaGetErrorString(error));                                    \
