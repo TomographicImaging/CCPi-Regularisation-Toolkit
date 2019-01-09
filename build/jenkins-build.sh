ddbc32fdcd4f0754b31786d00b2835223109a9d7
Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-01-09 11:17:45 +0000

    UPDATE: upload within jenkins build script, needs CCPI_CONDA_TOKEN

--- build/jenkins-build.sh
+++ build/jenkins-build.sh
@@ -14,7 +14,7 @@ else
   PATH=$PATH:./bin
 fi
 
-# presume that git clone is done before this script launch
+# presume that git clone is done before this script is launched, if not, uncomment
 # git clone https://github.com/vais-ral/CCPi-Regularisation-Toolkit
 conda install -y conda-build
 #export CIL_VERSION=0.10.2
@@ -31,3 +31,15 @@ conda build Wrappers/Python/conda-recipe
 # then need to call the same with --output 
 #- otherwise no build is done :-(, just fake file names are generated
 export REG_FILES=`conda build Wrappers/Python/conda-recipe --output`
+# REG_FILES variable should contain output files
+echo files created: $REG_FILES
+#upload to anaconda
+if [[ -n ${CCPI_CONDA_TOKEN}]]
+then
+  conda install anaconda-client
+  while read -r outfile; do
+    anaconda -v -t ${CCPI_CONDA_TOKEN}  upload $outfile --force --label dev
+  done <<< "$REG_FILES"
+else
+  echo CCPI_CONDA_TOKEN not defined, will not upload to anaconda.
+fi
