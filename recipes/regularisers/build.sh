d0052abf619d327e18791aeefd1b0a0a513c546b
vagrant <vagrant@localhost.localdomain>
2019-01-21 16:41:25 +0000

    UPDATE: build

--- recipes/regularisers/build.sh
+++ recipes/regularisers/build.sh
@@ -13,7 +13,7 @@ mkdir ${SRC_DIR}/build
 cp -rv ${RECIPE_DIR}/../../Core/ ${SRC_DIR}/build
 mkdir ${SRC_DIR}/build/build
 cd ${SRC_DIR}/build/build
-cmake -G "Unix Makefiles" -DCIL_VERSION="${CIL_VERSION}" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../Core
+cmake -G "Unix Makefiles" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../Core
 
 make -j2 VERBOSE=1
 make install
