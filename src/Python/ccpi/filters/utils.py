6577d4f3f29e39d83e52ce06a7f8d3ed367341eb
Casper da Costa-Luis <casper.dcl@physics.org>
2024-03-26 14:31:53 +0000

    fix ctypes usage

--- src/Python/ccpi/filters/utils.py
+++ src/Python/ccpi/filters/utils.py
@@ -1,31 +1,21 @@
 import platform
 import ctypes
 import os
-
-if platform.system() == 'Linux':
-    dll = 'libcilreg.so'
-elif platform.system() == 'Windows':
-    dll_file = 'cilreg.dll'
-    dll = ctypes.util.find_library(dll_file)
-elif platform.system() == 'Darwin':
-    dll = 'libcilreg.dylib'
-else:
-    raise ValueError('Not supported platform, ', platform.system())
-
-cilreg = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), dll))
+import warnings
 
 try:
-    if platform.system() == 'Linux':
-        gpudll = 'libcilregcuda.so'
-    elif platform.system() == 'Windows':
-        gpudll_file = 'cilregcuda.dll'
-        gpudll = ctypes.util.find_library(gpudll_file)
-    elif platform.system() == 'Darwin':
-        gpudll = 'libcilregcuda.dylib'
-    else:
-        raise ValueError('Not supported platform, ', platform.system())
+    pre = {'Linux': 'lib', 'Windows': '', 'Darwin': 'lib'}[platform.system()]
+    ext = {'Linux': '.so', 'Windows': '.dll', 'Darwin': '.dylib'}[platform.system()]
+except KeyError:
+    raise ValueError(f"unsupported platform: {platform.system()}")
 
-    cilregcuda = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), gpudll))
-except OSError as ose:
-    print(ose)
+_here = os.path.dirname(__file__)
+dll = f'{pre}cilreg{ext}'
+cilreg = ctypes.cdll.LoadLibrary(os.path.join(_here, dll))
+
+gpudll = f'{pre}cilregcuda{ext}'
+try:
+    cilregcuda = ctypes.cdll.LoadLibrary(os.path.join(_here, gpudll))
+except OSError as exc:
+    warnings.warn(str(exc), ImportWarning, stacklevel=2)
     cilregcuda = None
