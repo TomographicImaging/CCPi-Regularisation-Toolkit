1254f7f187e7dad1a5692140fd057c600e498a6e
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-03-26 21:49:28 +0000

    build cuda

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ ROBOCOPY /E "%RECIPE_DIR%\..\test" "%SRC_DIR%\test"
 if exist "%RECIPE_DIR%\..\build_proj" (
     rd /s /q "%RECIPE_DIR%\..\build_proj"
 )
-cmake -G "Visual Studio 16 2019" -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%SRC_DIR%\install" -DBUILD_CUDA=OFF
+cmake -G "Visual Studio 16 2019" -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%SRC_DIR%\install"
 cmake --build "%RECIPE_DIR%\..\build_proj" --target install --config RelWithDebInfo --verbose
 %PYTHON% -m pip install "%SRC_DIR%\src\Python" --verbose
 
