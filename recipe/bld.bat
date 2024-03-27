ba5dfb579eecc9767d9251538d8dfdca01be9f7f
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-03-27 08:57:51 +0000

    update bld.bat with instructions on CUDA and generator

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,9 @@ ROBOCOPY /E "%RECIPE_DIR%\..\test" "%SRC_DIR%\test"
 if exist "%RECIPE_DIR%\..\build_proj" (
     rd /s /q "%RECIPE_DIR%\..\build_proj"
 )
-cmake -G "Visual Studio 16 2019" -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%RECIPE_DIR%\..\install"
+:: add -G "Visual Studio 16 2019" to the cmake command to specify the generator
+:: add -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.4" to the cmake command to specify the CUDA toolkit
+cmake -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%RECIPE_DIR%\..\install"
 cmake --build "%RECIPE_DIR%\..\build_proj" --target install --config RelWithDebInfo --verbose
 %PYTHON% -m pip install "%SRC_DIR%\src\Python" --verbose
 
