f4054a01cbdb2c3bf21a227ceff836612f4afedc
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-03-27 09:14:15 +0000

    remove verbose output of cmake and pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ if exist "%RECIPE_DIR%\..\build_proj" (
 :: add -G "Visual Studio 16 2019" to the cmake command to specify the generator
 :: add -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.4" to the cmake command to specify the CUDA toolkit
 cmake -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%RECIPE_DIR%\..\install"
-cmake --build "%RECIPE_DIR%\..\build_proj" --target install --config RelWithDebInfo --verbose
-%PYTHON% -m pip install "%SRC_DIR%\src\Python" --verbose
+cmake --build "%RECIPE_DIR%\..\build_proj" --target install --config RelWithDebInfo
+%PYTHON% -m pip install "%SRC_DIR%\src\Python"
 
 if errorlevel 1 exit 1
