07a090f1d76edeb6fdee6b690135431605e11ddf
dkazanc <dkazanc@hotmail.com>
2024-05-13 21:02:09 +0100

    adding cupy kernels copy for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,9 @@ if exist "%RECIPE_DIR%\..\build_proj" (
     rd /s /q "%RECIPE_DIR%\..\build_proj"
 )
 
+mkdir "%SP_DIR%\ccpi\cuda_kernels"
+ROBOCOPY /E "%RECIPE_DIR%\..\src\Core\regularisers_GPU\cuda_kernels\" "%SP_DIR%\ccpi\"
+
 :: add -G "Visual Studio 16 2019" to the cmake command to specify the generator
 :: add -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.4" to the cmake command to specify the CUDA toolkit
 cmake -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%RECIPE_DIR%\..\install"
