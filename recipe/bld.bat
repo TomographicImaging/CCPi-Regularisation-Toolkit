158ed8693bfa8c578326a30e07ed9227a88c8221
Gemma Fardell <47746591+gfardell@users.noreply.github.com>
2024-09-13 16:05:27 +0100

    revert to RelWithDebInfo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,8 +8,8 @@ if exist "%RECIPE_DIR%\..\build_proj" (
 :: -T v142 specifies the toolset
 :: -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" to the cmake command to specify the CUDA toolkit version
 
-cmake -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -G "Visual Studio 16 2019" -T "v142" -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%RECIPE_DIR%\..\install"
-cmake --build "%RECIPE_DIR%\..\build_proj" --target install --config Release
+cmake -S "%SRC_DIR%" -B "%RECIPE_DIR%\..\build_proj" -G "Visual Studio 16 2019" -T "v142" -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%RECIPE_DIR%\..\install"
+cmake --build "%RECIPE_DIR%\..\build_proj" --target install --config RelWithDebInfo
 %PYTHON% -m pip install "%SRC_DIR%\src\Python"
 
 if errorlevel 1 exit 1
