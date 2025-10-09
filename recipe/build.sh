0a4b570cc98144d4555209928034ef9660a1e12b
Casper da Costa-Luis <casper.dcl@physics.org>
2025-10-09 15:49:53 +0100

    fix cuda build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ if test -d "$SRC_DIR/build_proj"; then
   rm -rf "$SRC_DIR/build_proj"
 fi
 
-cmake -S "$SRC_DIR" -B "$SRC_DIR/build_proj" -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="$CONDA_PREFIX" -DCMAKE_INSTALL_PREFIX="$SRC_DIR/install"
+cmake -S "$SRC_DIR" -B "$SRC_DIR/build_proj" -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DBUILD_CUDA=${BUILD_CUDA:-OFF} -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="$CONDA_PREFIX" -DCMAKE_INSTALL_PREFIX="$SRC_DIR/install"
 cmake --build "$SRC_DIR/build_proj" --target install
 $PYTHON -m pip install "$SRC_DIR/src/Python"
