1ca75413208d319bc1103ac63aba886a2a764390
Casper da Costa-Luis <casper.dcl@physics.org>
2025-10-09 22:34:31 +0100

    try fix cuda again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,6 +10,6 @@ if exist "%SRC_DIR%\build_proj" (
 
 cmake -S "%SRC_DIR%" -B "%SRC_DIR%\build_proj" -G "Visual Studio 16 2019" -T "v142" -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=%BUILD_CUDA% -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%SRC_DIR%\install"
 cmake --build "%SRC_DIR%\build_proj" --target install --config RelWithDebInfo
-%PYTHON% -m pip install "%SRC_DIR%\src\Python"
+%PYTHON% -m pip install -vv "%SRC_DIR%\src\Python"
 
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,4 @@ fi
 
 cmake -S "$SRC_DIR" -B "$SRC_DIR/build_proj" -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DBUILD_CUDA=${BUILD_CUDA:-OFF} -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="$CONDA_PREFIX" -DCMAKE_INSTALL_PREFIX="$SRC_DIR/install"
 cmake --build "$SRC_DIR/build_proj" --target install
-$PYTHON -m pip install "$SRC_DIR/src/Python"
+$PYTHON -m pip install -vv "$SRC_DIR/src/Python"
