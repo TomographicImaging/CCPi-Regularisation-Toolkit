ebd12d1b1be33323e84462778496874ce34e35dc
Casper da Costa-Luis <casper.dcl@physics.org>
2025-10-09 15:24:33 +0100

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 set -xe
 cp -rv "$RECIPE_DIR/../test" "$SRC_DIR/"
+if test -d "$SRC_DIR/build_proj"; then
+  rm -rf "$SRC_DIR/build_proj"
+fi
 
-cmake -S "$SRC_DIR" -B "$RECIPE_DIR/../build_proj" -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX="$RECIPE_DIR/../install"
-cmake --build "$RECIPE_DIR/../build_proj" --target install
+cmake -S "$SRC_DIR" -B "$SRC_DIR/build_proj" -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DBUILD_CUDA=ON -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="$CONDA_PREFIX" -DCMAKE_INSTALL_PREFIX="$SRC_DIR/install"
+cmake --build "$SRC_DIR/build_proj" --target install
 $PYTHON -m pip install "$SRC_DIR/src/Python"
