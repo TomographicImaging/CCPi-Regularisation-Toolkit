4b9734d3f1bb5e53229ab615d1e7c5e80fcf10f7
Casper da Costa-Luis <casper.dcl@physics.org>
2025-10-10 18:20:07 +0100

    fix conda build again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ if exist "%SRC_DIR%\build_proj" (
 :: -T v142 specifies the toolset
 :: -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" to the cmake command to specify the CUDA toolkit version
 
-cmake -S "%SRC_DIR%" -B "%SRC_DIR%\build_proj" -G "Visual Studio 16 2019" -T "v142" -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" -DBUILD_PYTHON_WRAPPERS=ON -DCONDA_BUILD=ON -DBUILD_CUDA=%BUILD_CUDA% -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%SRC_DIR%\install"
+cmake -S "%SRC_DIR%" -B "%SRC_DIR%\build_proj" -G Ninja -DBUILD_PYTHON_WRAPPERS=ON -DBUILD_CUDA=%BUILD_CUDA% -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%SRC_DIR%\install" %CMAKE_ARGS%
 cmake --build "%SRC_DIR%\build_proj" --target install --config RelWithDebInfo
 %PYTHON% -m pip install -vv "%SRC_DIR%\src\Python"
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ if test -d "$SRC_DIR/build_proj"; then
   rm -rf "$SRC_DIR/build_proj"
 fi
 
-cmake -S "$SRC_DIR" -B "$SRC_DIR/build_proj" -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DBUILD_CUDA=${BUILD_CUDA:-OFF} -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="$CONDA_PREFIX" -DCMAKE_INSTALL_PREFIX="$SRC_DIR/install"
+cmake -S "$SRC_DIR" -B "$SRC_DIR/build_proj" -G Ninja -DBUILD_PYTHON_WRAPPER=ON -DBUILD_CUDA=${BUILD_CUDA:-OFF} -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DLIBRARY_INC="$CONDA_PREFIX" -DCMAKE_INSTALL_PREFIX="$SRC_DIR/install" ${CMAKE_ARGS}
 cmake --build "$SRC_DIR/build_proj" --target install
 $PYTHON -m pip install -vv "$SRC_DIR/src/Python"
